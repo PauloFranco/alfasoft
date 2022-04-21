198bef27d56dd4ea6e1564a5054273f4c2ab4321
PauloFranco <paulimsantos@gmail.com>
2022-04-21 11:38:00 -0300

    Add user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' =>'Paulo',
+            'email' =>'paulo@paulo.com',
+            'password' =>'paulo',
+        ]);
     }
 }
--- resources/views/contacts/partials/action-buttons.blade.php
+++ resources/views/contacts/partials/action-buttons.blade.php
@@ -5,15 +5,15 @@
         'show'      => true,
     ])
 
-@include('common.action-button.delete', [
-    'action'    => route('contacts.destroy', compact('contact')),
-    'confirm'   => "Remover o contato \"{$contact->name}\"?",
-    'showLabel' => $showLabel,
-    'show'      => true,
-])
-
-@include('common.action-button.edit', [
-        'action'    => route('contacts.edit', compact('contact')),
+    @include('common.action-button.edit', [
+            'action'    => route('contacts.edit', compact('contact')),
+            'showLabel' => $showLabel,
+            'show'      => true,
+        ])
+        
+    @include('common.action-button.delete', [
+        'action'    => route('contacts.destroy', compact('contact')),
+        'confirm'   => "Remover o contato \"{$contact->name}\"?",
         'showLabel' => $showLabel,
         'show'      => true,
     ])
