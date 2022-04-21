27726efc65a110e1dae411f87151d99b975f8cbc
PauloFranco <paulimsantos@gmail.com>
2022-04-21 12:57:19 -0300

    botao criar somente visível para usuários logados

--- resources/views/contacts/partials/card-list.blade.php
+++ resources/views/contacts/partials/card-list.blade.php
@@ -2,10 +2,12 @@
     @forelse($contacts as $contact)
         @include('contacts.partials.card', compact('contact'))
     @empty
-        <li class="list-group-item text-center text-muted">
-            Use o botão
-            @include('contacts.partials.create-button', [ 'show' => true ])
-            para cadastrar o primeiro contato.
-        </li>
+    @auth
+            <li class="list-group-item text-center text-muted">
+                Use o botão
+                @include('contacts.partials.create-button', [ 'show' => true ])
+                para cadastrar o primeiro contato.
+            </li>
+        @endauth
     @endforelse
 </ul>
